64ed3ed8ca0944ce5dbd550150c10546d72c0878
BrenoLeiriaoNeto <bleiriaoneto@icloud.com>
2026-05-27 17:35:12 -0300

    feat(garage-api): implement dependency injection and Fipe data source for garage feature

---
+++ lib/src/core/di/injection_container.dart
@@ -0,0 +1,25 @@
+import 'package:get_it/get_it.dart';
+
+import '../../core/core_exports.dart';
+import '../../features/garage/data_exports.dart';
+import '../../features/garage/domain_exports.dart';
+import '../../features/garage/presentation/controllers/garage_controller.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initDependencies() async {
+  sl.registerLazySingleton<HttpClient>(() => HttpClient());
+
+  sl.registerLazySingleton<IFipeDataSource>(
+    () => FipeDataSource(sl<HttpClient>()),
+  );
+  sl.registerLazySingleton<IGarageRepository>(
+    () => GarageRepository(sl<IFipeDataSource>()),
+  );
+  sl.registerLazySingleton<GetBrandsUseCase>(
+    () => GetBrandsUseCase(sl<IGarageRepository>()),
+  );
+  sl.registerFactory<GarageController>(
+    () => GarageController(sl<GetBrandsUseCase>()),
+  );
+}
